7ec64256025d24601f1665b5fa772c94b5e5a6e2
demmysabel <ekprokhorenko@gmail.com>
2022-03-11 13:36:39 +0300

    Another commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 
 public class Main<totalSum> {
     public static void main(String[] args) {
-        int addingSum = 500;
+        int addingSum = 1000;
         int basicSum = 100;
-        int bonus = addingSum / 100;
-        int presentSum = bonus + addingSum + basicSum;
         int totalSum;
         if (addingSum >= 1000) {
+            int bonus = addingSum / 100;
+            int presentSum = bonus + addingSum + basicSum;
             totalSum = presentSum;
         } else {totalSum=addingSum+basicSum;
 
